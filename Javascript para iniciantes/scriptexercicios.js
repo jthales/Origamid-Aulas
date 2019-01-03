75d3d134e173345101e5caeeac8536f34ec4efcb
Thales Henrique <thaleshenrique38@live.com>
2019-01-03 10:09:17 -0200

    Update scriptexercicios.js

---
+++ Javascript para iniciantes/scriptexercicios.js
@@ -0,0 +1,10 @@
+function verdadeiroOuFalso(x) {
+    if(x == '0') {
+        return 'Função verdadeira';
+    }
+    else if(x == '1'){
+        return 'Função um';
+    }
+}
+
+console.log('Insira console.log(verdadeiroOuFalso(e o número desejado)')
