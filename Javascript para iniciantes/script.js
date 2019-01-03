bdae658de99a1f94c60a388b1764c246831236be
Thales Henrique <thaleshenrique38@live.com>
2019-01-03 08:58:03 -0200

    Update script.js

--- Javascript para iniciantes/script.js
+++ Javascript para iniciantes/script.js
@@ -1,6 +1,12 @@
-var possuiGraducao = true;
+var possuiGraduacao = true;
+var possuiDoutorado = false;
 
-if(possuiGraducao) {
-    console.log('Possui graduação')
-else
+if(possuiDoutorado) {
+    console.log('Possui graduação e dout');
+}
+else if(possuiGraduacao){
+    console.log('Possui graduação, mas não possui doutorado');
+}
+else {
+    console.log('Não possui graduação');
 }
